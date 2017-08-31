0501a00236ca8bc95267f684f50ffb7f8da66dbc
little-lemon <766019882@qq.com>
2017-09-01 00:18:47 +0800

    3

--- tmp/createPage.js
+++ tmp/createPage.js
@@ -7,86 +7,97 @@ define(['../common','exports','jquery'],function (common,exports,$) {
 	 * @param {Object} oPageinfo 分页相关信息	传入一个对象
 	 * 
 	 */		
-	var page = function(options){
-		this.defaults ={
+	var page = function(){
+		this.pageOpts ={
 			currpage:1,         //当前页数
 			pagerow:[8,10,15],  //每页显示条数
 			pageCount:1,        //总页数
 			total:1,            //总数
 			limit:8,            //当前选则的每页显示条数
-			url:'../data/terminallog.json',              //获取数据的地址
+			//url:'../data/terminallog.json',              //获取数据的地址
 			el:'.pagecontent'  //分页数据插入位置
 		}
-				
-		this.parameter = $.extend(true,this.defaults,options);
-		
-		console.log(this.parameter);
 	}
 	
 	
-	page.prototype.init = function(){
+	page.prototype.init = function(url,pageOpts,gridOpts){
+		this.url = url;
+		this.pageOpts = $.extend(true,this.pageOpts,pageOpts);
+		this.gridOpts = gridOpts;
 		this.setData();
-		return this;
 	}
+	
 	
 	page.prototype.setData = function(){
-		this.getGridData(this.parameter.currpage,this.parameter.limit);
-		this.getPageData(this.parameter.url,this.parameter.createPage);
-		return this;
-	}
-	
-	page.prototype.getGridData = function(){
-		
-	}
-	
-	//u获取数据的地址
-	page.prototype.getPageData = function(u,createPage){
-        var that = this;
+		var that = this;
         $.ajax({
- 			url: u,
+ 			url: that.url,
  			success: function (d) {
- 				that.parameter.total = d.data.total;
-	            that.parameter.pageCount = Math.ceil(that.parameter.total/ that.parameter.limit);
- 				createPage(that);
+ 				that.pageOpts.total = d.data.total;
+	            that.pageOpts.pageCount = Math.ceil(that.pageOpts.total/ that.pageOpts.limit);
+	            var star = parseInt(that.pageOpts.currpage-1)*parseInt(that.pageOpts.limit);
+	            var end =  star + parseInt(that.pageOpts.limit);
+	            that.gridOpts.data = d.data.rows.slice(star,end);
+	            that.createGrid();
+ 				that.createPage();
  			}
  		});
- 		return this;
 	}
 	
+	page.prototype.createGrid = function(){
+		var gridData = this.gridOpts;
+		var result = '<table class="table table-responsive">';
+   		result += '<tr class="tabletitle"><th width="3%"></th>';
+   		for(var i=0,len=gridData.colModel.length;i<len;i++){
+   			result += '<th>'+ gridData.colModel[i].display +'</th>'
+   		}
+   		result += '<th width="3%"></th>';
+   		for(var i=0,len=gridData.data.length;i<len;i++){
+   			result += '<tr class="trhover"><td></td>';
+   			for(var j in gridData.data[i]) {
+		        result += '<td>'+gridData.data[i][j]+'</td>';
+		    }    
+   			result += '<td></td></tr>';
+   		}
+   		result += '</table>';
+   		
+   		$(gridData.el).html(result)
+	}
 	
-	page.prototype.createPage = function(pageInfo){
+	page.prototype.createPage = function(){
+		pageInfo = this;
 		//	左侧分页内容
 		var lpageCont = '';
 		lpageCont += '<div class="text-left"> 每页显示 ';		
-		lpageCont += '<span id="crow">'+pageInfo.parameter.limit+'</span>';
+		lpageCont += '<span id="crow">'+pageInfo.pageOpts.limit+'</span>';
 		lpageCont += '<a><img src="images/pagedown.png" class="pagedown pd1"/></a>';
-		lpageCont += '<ul class="pagedownlist" id="colcount" style="margin-top:'+ -(pageInfo.parameter.pagerow.length*18 +12 + 16) +'px;">';		
+		lpageCont += '<ul class="pagedownlist" id="colcount" style="margin-top:'+ -(pageInfo.pageOpts.pagerow.length*18 +12 + 16) +'px;">';		
 		var rowlist = '';		
-		for(var i=0,len=pageInfo.parameter.pagerow.length;i<len;i++){
-			rowlist += '<li>'+pageInfo.parameter.pagerow[i]+'</li>';
+		for(var i=0,len=pageInfo.pageOpts.pagerow.length;i<len;i++){
+			rowlist += '<li>'+pageInfo.pageOpts.pagerow[i]+'</li>';
 		}
 		rowlist += '</ul> 条 | 共 ';
 		lpageCont += rowlist;		
-		lpageCont += '<span>'+pageInfo.parameter.total+'</span> 条 </div>';
+		lpageCont += '<span>'+pageInfo.pageOpts.total+'</span> 条 </div>';
 		
 		//	右侧分页内容
 		var rpageCont = '';
 		rpageCont += '<div class="text-right"> 第 ';
-		rpageCont += '<span id="cpage">'+pageInfo.parameter.currpage+'</span> ';
+		rpageCont += '<span id="cpage">'+pageInfo.pageOpts.currpage+'</span> ';
 		rpageCont += '<a><img src="images/pagedown.png" class="pagedown pd2"/></a> '		
-		rpageCont += '<ul class="pagedownlist" id="pagecount" style="margin-top:'+ -(pageInfo.parameter.pageCount*18 +12 +16) +'px;">';		
+		rpageCont += '<ul class="pagedownlist" id="pagecount" style="margin-top:'+ -(pageInfo.pageOpts.pageCount*18 +12 +16) +'px;">';		
 		var pageList = '';
-		for(var i = 1,len=pageInfo.parameter.pageCount; i <= len; i++){
+		for(var i = 1,len=pageInfo.pageOpts.pageCount; i <= len; i++){
 			pageList += '<li>'+i+'</li>';
 		}
 		pageList += '</ul>页 | 共 ';
 		rpageCont += pageList;
-		rpageCont += '<span id="sumpage">'+pageInfo.parameter.pageCount+'</span> 页 ';
+		rpageCont += '<span id="sumpage">'+pageInfo.pageOpts.pageCount+'</span> 页 ';
 		rpageCont += '<a class="prepage">上一页</a><a class="nextpage">下一页</a></div>';
 		
-		$(pageInfo.parameter.el).html(lpageCont + rpageCont);
-		pageInfo.parameter.pageLeftEventInit('.pd1');
-		pageInfo.parameter.pageRightEventInit('.pd2');
+		$(pageInfo.pageOpts.el).html(lpageCont + rpageCont);
+		pageInfo.pageLeftEventInit('.pd1');
+		pageInfo.pageRightEventInit('.pd2');
 		return this;
 	}
 	
@@ -96,6 +107,7 @@ define(['../common','exports','jquery'],function (common,exports,$) {
 	}
 	
 	page.prototype.pageRightEventInit = function(el,options){
+		var that = this;
 		this.pagedownSwitch(el,options);
 		//	上下页按钮点击事件相关变量
 		var btnpre = $('.prepage'),
@@ -108,12 +120,17 @@ define(['../common','exports','jquery'],function (common,exports,$) {
 			cpage = cpage - 1;
 			cpage = cpage <= 0 ? 1 : cpage;
 			$('#cpage').html(cpage);
+			that.pageOpts.currpage = cpage;
+			that.setData();
+			
 		});
 		//	下页点击
 		btnnext.click(function(){				
 			cpage = cpage + 1;
 			cpage = cpage >= sumpage ? sumpage : cpage;
 			$('#cpage').html(cpage);
+			that.pageOpts.currpage = cpage;
+			that.setData();
 		});
 		return this;
 	}
@@ -133,28 +150,33 @@ define(['../common','exports','jquery'],function (common,exports,$) {
 		});
 		
 		// 下拉按钮点击完
-		$(el).parent().next().children().click(function(){
+		$(el).parent().next().children().on('click',function(){
 			$(this).parent().slideUp(opts.speed);
 			//	根据ID改变当前显示的分页
 			if( $(this).parent()[0].id == 'colcount' ){
 				$('#crow').html($(this).text());
-				var limit = parseInt(that.limit);
+				var limit = parseInt(that.pageOpts.limit);
 				var currpage = parseInt($('#cpage').text());
 				//that.currpage = Math.ceil( (limit*currpage)/$(this).text() );
-				that.currpage = Math.ceil( (limit*currpage)/$(this).text() );
-				console.log(that.currpage);
-				that.limit = $(this).text();
+				var tmp = $(that.gridOpts.el).find('tr');
+				//var tmp = tableEment.(tr);
+				console.log(tmp);
+				that.pageOpts.currpage = Math.ceil( (limit*currpage)/$(this).text() );
+				console.log(that.pageOpts.currpage);
+				that.pageOpts.limit = $(this).text();
 				that.setData();
 			}else{
 				$('#cpage').html($(this).text());
 				cpage = parseInt($('#cpage').text());
+				that.pageOpts.currpage = cpage;
+				that.setData();
 			}
 		});
 		
 		return this;
 	}
 	
-	 
+	//new page().init();
 	
 	exports.page = new page();
 
--- tmp/terminallog.js
+++ tmp/terminallog.js
@@ -9,17 +9,37 @@ define(['../common'], function(common) {
 		'jsapp/grid'
 	], function(echarts, $, oPage,oSlide,oCtime,oGrid){
 		
-		var options = {
+		var url = '../data/terminallog.json';              //获取数据的地址
+		/*分页的数据设置*/
+		var pageOpts = {
 			currpage:1,         //当前页数
 			pagerow:[8,10,15],  //每页显示条数
 			pageCount:1,        //总页数
-			total:1,            //总数
+			total:12,            //总数
 			limit:8,            //当前选则的每页显示条数
-			url:'../data/terminallog.json',              //获取数据的地址
 			el:'.pagecontent'  //分页数据插入位置
-		}
+		};
+		/*表格数据设置*/
+		var gridOpts = {
+	   		colModel:[
+	   			{display:"序号",name:'id'},
+	   			{display:"应用名称",name:'name'},
+	   			{display:"发生时间",name:'dt'},
+	   			{display:"用户",name:'username'},
+	   			{display:"用户IP",name:'userip'},
+	   			{display:"源IP",name:'sip'},
+	   			{display:"目标IP",name:'dip'},
+	   			{display:"发送流量",name:'sflow'},
+	   			{display:"接收流量",name:'rflow'},
+	   			{display:"源端口",name:'sport'},
+	   			{display:"目的端口",name:'dport'},
+	   			{display:"协议",name:'protocol'},
+	   			{display:"URL",name:'url'}
+	   		],
+	   		el:'#table'
+		};
 		
-		oPage.page(options).init()
+		oPage.page.init(url,pageOpts,gridOpts);
 		//	查询区域伸缩
 		oSlide.Slide('#showsearch','.hidecont');
 		
@@ -47,7 +67,7 @@ define(['../common'], function(common) {
 	  
 	    //表格相关信息
 	    var OGridinfo = {
-	   		data:gridData,
+	   		/*data:gridData,
 	   		colModel:[
 	   			{display:"序号",name:'id'},
 	   			{display:"应用名称",name:'name'},
@@ -62,11 +82,11 @@ define(['../common'], function(common) {
 	   			{display:"目的端口",name:'dport'},
 	   			{display:"协议",name:'protocol'},
 	   			{display:"URL",name:'url'}
-	   		]
+	   		]*/
 	    }
 		
-		var grid = oGrid.createGrid(OGridinfo);
-	    $('#table').html(grid);
+		//var grid = oGrid.createGrid(OGridinfo);
+	   // $('#table').html(grid);
 		
 		/*
 		//	分页相关信息
